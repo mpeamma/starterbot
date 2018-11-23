17ece907f7efc7d82c55360561e843609e3a42be
Michael Eamma <mpeamma@gmail.com>
2018-11-23 15:34:18 -0600

    Added mongo integration

--- starterbot.py
+++ starterbot.py
@@ -2,14 +2,26 @@ import os
 import time
 import re
 from slackclient import SlackClient
+from pymongo import MongoClient
+import json
 
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+with open('config.json', 'r') as f:
+    data = f.read()
+config = json.loads(data)
+
+slack_client = SlackClient(config['SLACK_BOT_TOKEN'])
 starterbot_id = None
 
 RTM_READ_DELAY = 1
 EXAMPLE_COMMAND = "do"
 SCOREBOARD_COMMAND = "scoreboard"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+connection = MongoClient(config['db_url'], config['db_port'])
+db = connection[config['db_name']]
+db.authenticate(config['db_user'], config['db_pass'])
+
+score = db.score
 
 def parse_bot_commands(slack_events):
     for event in slack_events:
@@ -21,7 +33,7 @@ def parse_bot_commands(slack_events):
 
 def parse_direct_mention(message_text):
     matches = re.search(MENTION_REGEX, message_text)
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None, None)
+    return (matches.group(1), matches.group(2).strip()) if matches else ("", "")
 
 def get_user_info(user_id):
     return slack_client.api_call(
@@ -35,15 +47,25 @@ def handle_scoreboard(command, channel, author):
     userinfo = get_user_info(user_id)
     authorinfo = get_user_info(author)
     tokens = message.split()
-    try:
-        amount = float(tokens[0])
-        description = " ".join(tokens[1:]) if len(tokens) > 1 else ""
-        return "The user mentioned is: " + userinfo["user"]["real_name"] + \
-           ". \nThe amount is: " + str(amount) +\
-           ". \nThe description is: " + description +\
-           ". \nThe author is " + authorinfo["user"]["real_name"]
-    except:
-        return "Syntax: @User <amount> <description>"
+    if len(tokens) > 0:
+        try:
+            amount = float(tokens[0])
+            description = " ".join(tokens[1:]) if len(tokens) > 1 else ""
+            entity = {
+                "giver": authorinfo["user"],
+                "receiver": userinfo["user"],
+                "amount": amount,
+                "description": description 
+            }
+            score.insert(entity)
+            return "%s has given %.2f points to %s for %s" % (authorinfo["user"]["real_name"], amount, userinfo["user"]["real_name"], description)
+        except:
+            return "Syntax: @User <amount> <description>"
+    else:
+        message = "```%-20s %s\n" % ("User", "Score")
+        for x in score.aggregate([ {"$group": { "_id": "$receiver.id", "total": { "$sum": "$amount" }, "name": { "$first": "$receiver.real_name" } } }]):
+            message += "%-20s %.2f\n" % (x["name"], x["total"])
+        return message + "```"
 
 def handle_command(command, channel, author):
     default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
